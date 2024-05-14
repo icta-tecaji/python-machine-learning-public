46a4a2610eac5588a76b62588a57b59a8bba484f
anzeglusic <anze.glusic@gmail.com>
2024-05-14 18:14:32 +0200

    Predavanje 6

--- 03_Feature_Engineering/helpers/datasets.py
+++ 03_Feature_Engineering/helpers/datasets.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import os
 from scipy import signal
-from sklearn.datasets import load_boston
+# from sklearn.datasets import load_boston
 from sklearn.preprocessing import MinMaxScaler, PolynomialFeatures
 from sklearn.datasets import make_blobs
 
